8b67f1d07f82b8c62fd108b9df054aad139b6442
JAWorsfold <JAWorsfold@users.noreply.github.com>
2018-12-01 15:18:22 +0000

    Initialised Coursera repo for Algorithms, Part 1.

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ *  Name: Joseph Worsfold
+ *  Date: 01/12/2018
+ *  Description: Algoriths Part 1 - src.Percolation Problem
+ **************************************************************************** */
+
+public class Percolation {
+
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        return;
+    }
+
+    // create n-by-n grid, with all sites blocked
+    public void open(int row, int col) {
+        return;
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return;
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return;
+    }
+
+    public static void main(String[] args) {
+        return;
+    }
+}
